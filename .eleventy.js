69aae13b9649d2d316253af35b5bbe168b20e81a
rooc <vadym.hrachov@pm.me>
2020-10-18 02:33:14 +0300

    env

--- .eleventy.js
+++ .eleventy.js
@@ -4,35 +4,26 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.setUseGitIgnore(false);
 
 	eleventyConfig.addWatchTarget("./static/img");
-	eleventyConfig.addPassthroughCopy({"./static/img":"./img/"});
-	
+	eleventyConfig.addPassthroughCopy({
+		"./static/img": "./img/"
+	});
+
 	eleventyConfig.addWatchTarget("./_tmp/styles.css");
-	eleventyConfig.addPassthroughCopy({"./_tmp/styles.css": "./css/styles.css"});
-	
+	eleventyConfig.addPassthroughCopy({
+		"./_tmp/styles.css": "./css/styles.css"
+	});
+
 	eleventyConfig.addWatchTarget("./static/js/");
-	eleventyConfig.addPassthroughCopy({"./static/js/": "./js/"});
-	
-	eleventyConfig.addPassthroughCopy({"./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js",});
+	eleventyConfig.addPassthroughCopy({
+		"./static/js/": "./js/"
+	});
+
+	eleventyConfig.addPassthroughCopy({
+		"./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js",
+	});
 
 	eleventyConfig.addShortcode("version", function() {
 		return String(Date.now());
-	});
-
-	eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-		if (
-			process.env.ELEVENTY_PRODUCTION &&
-			outputPath &&
-			outputPath.endsWith(".html")
-		) {
-			let minified = htmlmin.minify(content, {
-				useShortDoctype: true,
-				removeComments: true,
-				collapseWhitespace: true,
-			});
-			return minified;
-		}
-
-		return content;
 	});
 
 	return {
