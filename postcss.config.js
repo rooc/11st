8a0a622bdd451988d4a5b046375f997d40db88fd
rooc <vadym.hrachov@pm.me>
2020-10-18 12:33:48 +0300

    postcss import nested

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
 	plugins: [
+		require("postcss-import"),
+		require('postcss-nested'),
 		require(`tailwindcss`)(`./tailwind.config.js`),
 		require(`autoprefixer`),
 		...(process.env.NODE_ENV === "production" ?
