87748dc7662f024ddbe81a776dc7d4b3f2a1fbba
rooc <vadym.hrachov@pm.me>
2020-10-19 12:34:52 +0300

    parcel setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,9 @@
 const htmlmin = require("html-minifier");
 
 module.exports = function(eleventyConfig) {
-	eleventyConfig.setUseGitIgnore(false);
 
 	eleventyConfig.addWatchTarget("./static/img");
 	eleventyConfig.addPassthroughCopy({"./static/img": "./img/"});
-
-	eleventyConfig.addWatchTarget("./static/js/");
-	eleventyConfig.addPassthroughCopy({"./static/js/": "./js/"});
-
-	eleventyConfig.addPassthroughCopy({
-		"./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js",
-	});
 
 	eleventyConfig.addShortcode("version", function() {
 		return String(Date.now());
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,8 @@
+import './libs/alpine.js';
+import log from "./modules/module.js";
+
 function main() {
 	console.log("Yo Man Man");	
 }
+
+log("Yo Man Man!");
