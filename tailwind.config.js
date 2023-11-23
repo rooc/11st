630ed30f72dabd284de01a54b0948881179c11b9
rooc <vadim.grachev@gmail.com>
2023-11-23 13:36:15 +0200

    update and variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,48 @@
 module.exports = {
-	content: [
-		'./dist/**/*.{html,js}',
-	],
+	content: ["./dist/**/*.{html,js}"],
 	theme: {
 		extend: {
 			colors: {
-				transparent: 'transparent',
-				'tahiti': 'hsla(245, 21%, 14%, 1)',
+				transparent: "transparent",
+				tahiti: "hsla(245, 21%, 14%, 1)",
+			},
+		},
+		variables: {
+			DEFAULT: {
+				sizes: {
+					small: "1rem",
+				},
+				colors: {
+					red: {
+						50: "red",
+					},
+				},
+			},
+			".container": {
+				colors: {
+					red: {
+						50: "indigo",
+					},
+				},
+			},
+		},
+		darkVariables: {
+			DEFAULT: {
+				colors: {
+					red: {
+						50: "blue",
+					},
+				},
+			},
+			".container": {
+				colors: {
+					red: {
+						50: "green",
+					},
+				},
 			},
 		},
 	},
-	plugins: [],
-}
+	darkMode: "class",
+	plugins: [require("@mertasan/tailwindcss-variables")],
+};
